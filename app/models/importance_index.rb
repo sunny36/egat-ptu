64def6e5de8df1841086cc5ca0a41abd35a3184e
Somchok Sakjiraphong <s@sunny.in.th>
2010-08-28 22:33:19 +0700

    Bus voltage hv in views.

--- app/models/importance_index.rb
+++ app/models/importance_index.rb
@@ -3,11 +3,7 @@ class ImportanceIndex < ActiveRecord::Base
   attr_accessor :bus_voltage_lv, 
                 :system_fault_level_hv, :system_fault_level_hv, 
                 :mva_hv, :mva_lv
-  before_save :calculate_bus_voltage
+ 
   
-  private 
-  
-  def calculate_bus_voltage
-    self.bus_voltage = @bus_voltage_hv
-  end
+
 end
